ee12863c3aec6907dbdc4a458386e5a1a6385828
imabby0508 <imabby0508@gmail.com>
2022-09-30 14:59:57 +0800

    restructure API helper

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -1,74 +1,47 @@
 import { apiHelper } from "./../utils/helpers";
-const getToken = () => localStorage.getItem("token");
 
 export default {
   restaurants: {
     create({ formData }) {
-      return apiHelper.post("/admin/restaurants", formData, {
-        headers: { Authorization: `Bearer ${getToken()}` },
-      });
+      return apiHelper.post("/admin/restaurants", formData);
     },
     get() {
-      return apiHelper.get("/admin/restaurants", {
-        headers: { Authorization: `Bearer ${getToken()}` },
-      });
+      return apiHelper.get("/admin/restaurants");
     },
     getDetails({ restaurantId }) {
-      return apiHelper.get(`/admin/restaurants/${restaurantId}`, {
-        headers: { Authorization: `Bearer ${getToken()}` },
-      });
+      return apiHelper.get(`/admin/restaurants/${restaurantId}`);
     },
     update({ restaurantId, formData }) {
-      return apiHelper.put(`/admin/restaurants/${restaurantId}`, formData, {
-        headers: { Authorization: `Bearer ${getToken()}` },
-      });
+      return apiHelper.put(`/admin/restaurants/${restaurantId}`, formData);
     },
     delete({ restaurantId }) {
-      return apiHelper.delete(`/admin/restaurants/${restaurantId}`, {
-        headers: { Authorization: `Bearer ${getToken()}` },
-      });
+      return apiHelper.delete(`/admin/restaurants/${restaurantId}`);
     },
   },
   categories: {
     create({ name }) {
       return apiHelper.post(
         "/admin/categories",
-        { name },
-        {
-          headers: { Authorization: `Bearer ${getToken()}` },
-        }
-      );
+        { name });
     },
     get() {
-      return apiHelper.get("/admin/categories", {
-        headers: { Authorization: `Bearer ${getToken()}` },
-      });
+      return apiHelper.get("/admin/categories");
     },
     update({ categoryId, name }) {
       return apiHelper.put(
         `/admin/categories/${categoryId}`,
-        { name },
-        {
-          headers: { Authorization: `Bearer ${getToken()}` },
-        }
-      );
+        { name });
     },
     delete({ categoryId }) {
-      return apiHelper.delete(`/admin/categories/${categoryId}`, {
-        headers: { Authorization: `Bearer ${getToken()}` },
-      });
+      return apiHelper.delete(`/admin/categories/${categoryId}`);
     },
   },
   users: {
     get() {
-      return apiHelper.get("/admin/users", {
-        headers: { Authorization: `Bearer ${getToken()}` },
-      });
+      return apiHelper.get("/admin/users");
     },
     update({ userId, isAdmin }) {
-      return apiHelper.put(`/admin/users/${userId}`, {isAdmin}, {
-        headers: { Authorization: `Bearer ${getToken()}` },
-      });
+      return apiHelper.put(`/admin/users/${userId}`, {isAdmin});
     },
   },
 };
